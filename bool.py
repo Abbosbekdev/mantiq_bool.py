330121f232f8c64c45061e7942514abf579bc5f4
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-02 10:09:25 +0500

    Create bool.py

---
+++ bool.py
@@ -0,0 +1,257 @@
+#1-masala
+
+#a=int(input("a = "))
+#son=bool
+#son = a>0
+#print(son)
+
+#2-masala
+
+#a=int(input('a = '))
+#son=bool
+#son = a%2==1
+#print(son)
+
+#3-masala
+
+#a=int(input('a = '))
+#son=bool
+#son = a%2==0
+#print(son)
+
+#4-masala
+
+#a,b=int(input('a = ')),int(input('b = '))
+#son = bool
+#son= a>2 and b<=3
+#print(son)
+
+#5-masala
+
+#a,b=int(input('a = ')),int(input('b= '))
+#son= bool(a>0 and b<-2)
+#+print(son)
+
+#6-masala
+
+#a,b,c=int(input('a = ')),int(input('b = ')),int(input('c = '))
+#son = bool
+#son = a<=b<=c
+#print(son)
+#7-masala
+
+#a,b,c=int(input('a = ')),int(input('b = ')),int(input('c = '))
+#son = bool
+#son = a<b<c
+#print(son)
+
+#8-masala
+
+#a,b=int(input()),int(input())
+#son = bool
+#son = a%2==1 and b%2==1
+#print(son)
+
+#9-masala
+
+#a,b=int(input()),int(input())
+#son = bool
+#son = a%2==1 or b%2==1
+#print(son)
+
+#10-masala
+
+#a,b=int(input()),int(input())
+#son= bool
+#son = a%2==1 and b%2==0 or a%2==0 and b%2==1
+#print(son)
+
+#11-masala
+
+#a,b=int(input()),int(input())
+#son= bool
+#son = a%2==1 and b%2==1 or a%2==1 and b%2==1
+#print(son)
+
+#12-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son = a>0 and b>0 and c>0
+#print(son)
+
+#13-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son = a>0 or b>0 or c>0
+#print(son)
+
+#14-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son =a>0 and b<0 and c<0 or b>0 and a<0 and c<0 or c>0 and b<0 and a<0
+#print(son)
+
+#15-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son =a>0 and b>0 and c<0 or b>0 and a<0 and c>0 or c>0 and b<0 and a>0
+#print(son)
+
+#16-masala
+
+#a = int(input())
+#son=bool
+#son= a>=10 and a<100 and a%2==0
+
+#17-masala
+
+#a = int(input())
+#son = bool
+#son =a>=100 and a<1000 and a%2==0
+#print(son)
+
+#18-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son = a==b or b==c and a!=c or a==c or b==c and a!=b or a==b or a==c and b!=c
+#print(son)
+
+#19-masala
+
+#a,b,c=int(input()),int(input()),int(input())
+#son = bool
+#son = a+b==0 or b+c==0 and a+c!=0 or a+c==0 or b+c==0 and a+b!=0 or a+b==0 or a+c==0 and b+c!=0
+#print(son)
+
+#20-masala
+
+#a=int(input())
+#son = bool
+#son = a//100!=a%10 and a%100//10 != a%10 and a//100!=a//10%10
+#print(son)
+
+#21-masala
+
+#a=int(input())
+#son = bool
+#son = a//100 < a//10%10 < a%10
+#print(son)
+
+#22-masala
+
+#a=int(input())
+#son = bool
+#son = a//100 < a//10%10 < a%10 or a//100 > a//10%10 > a%10
+#print(son)
+
+#23-masala
+
+#a=int(input())
+#son = bool
+#on = a//100 == a%10 and a//100 != a//10%10
+#print(son)
+
+#24-masala
+
+# a,b,c=int(input()),int(input()),int(input())
+# son = bool
+# son = b*b-4*a*c>=0
+# print(son)
+
+#25-masala
+
+# x,y=int(input()),int(input())
+# son = bool
+# son = x<0 and y>0
+# print(son)
+
+# 26-masala
+
+# x,y=int(input()),int(input())
+# son = bool
+# son = x>0 and y<0
+# print(son)
+
+# 27-masala
+
+# x,y=int(input()),int(input())
+# son = bool
+# son = x<0 and y>0 or x<0 and y<0
+# print(son)
+
+
+# 28-masala
+
+# x,y=int(input()),int(input())
+# son = bool
+# son = x>0 and y>0 or x<0 and y<0
+# print(son)
+
+#30-masala
+
+# a,b,c=int(input()),int(input()),int(input())
+# son = bool
+# son = a==c and c==b
+# print(son)
+
+#31-masala
+
+# a,b,c=int(input()),int(input()),int(input())
+# son = bool
+# son = a==c and c!=b or a==b and a!=c or b==c and b!=a
+# print(son)
+
+#32-masala
+# import math
+#
+# a,b,c=int(input()),int(input()),int(input())
+# son = bool
+# son = a==math.sqrt(b*b+c*c) or b==math.sqrt(a*a+b*b) or c==math.sqrt(a*a+b*b)
+# print(son)
+
+#33-masala
+
+# a,b,c=int(input()),int(input()),int(input())
+# son = bool
+# son = a+b >c and c>abs(a-b) or a+c >b and b>abs(a-c) or c+b >a and a>abs(c-b)
+# print(son)
+
+#34-masala
+
+# x,y = int(input()),int(input())
+# son = bool
+# son = (x+y)%2==1
+# print(son)
+
+#35-masala
+
+# x,y=int(input()),int(input())
+# x1,y1=int(input()),int(input())
+# son =bool
+# son = (x+y)%2 ==0 and (x1+y1)%2==0 or (x+y)%2==1 and (x1+y1)%2==1
+# print(son)
+
+#36-masala
+
+# x,y=int(input()),int(input())
+# x1,y1=int(input()),int(input())
+# son =bool
+# son = x1==x or y1==y
+# print(son)
+
+#37-masala
+
+# x,y=int(input()),int(input())
+# x1,y1=int(input()),int(input())
+# son = bool
+# son = x1 == x+1 or x1 == x-1 or y1==y+1 or y1==y-1
+# print(son)
+
+#38-masala
+
+
